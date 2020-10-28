d20dd48ec54f6fd5e6d9e0fa6d2e74b4f0b5965c
Peter <p.dyumin@dunice.net>
2020-10-28 22:10:08 +0300

    Revert schema changes

--- database/schemas/common.go
+++
@@ -1,8 +0,0 @@
-package schemas
-
-// Common schema fields
-type CommonFields struct {
-	Created int64  `json:"created"`
-	ID      string `json:"id,omitempty" bson:"_id,omitempty"`
-	Updated int64  `json:"updated"`
-}
