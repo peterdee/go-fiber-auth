38401c97278413c95309e9455dd31970bff9f54d
Peter <p.dyumin@dunice.net>
2020-10-29 23:18:47 +0300

    Fix file uploading

--- apis/account/update-avatar.controller.go
+++ apis/account/update-avatar.controller.go
@@ -4,55 +4,55 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"go-fiber-auth/configuration"
-	// . "go-fiber-auth/database"
-	// . "go-fiber-auth/database/schemas"
+	. "go-fiber-auth/database"
+	. "go-fiber-auth/database/schemas"
 	"go-fiber-auth/utilities"
 )
 
 // Update account avatar
 func updateAvatar(ctx *fiber.Ctx) error {
 	// get User ID and convert it to the ObjectID format
-	// stringId := fmt.Sprintf("%v", ctx.Locals("UserId"))
-	// userId, primitiveError := primitive.ObjectIDFromHex(stringId)
-	// if primitiveError != nil {
-	// 	return utilities.Response(utilities.ResponseParams{
-	// 		Ctx:    ctx,
-	// 		Info:   configuration.ResponseMessages.InternalServerError,
-	// 		Status: fiber.StatusInternalServerError,
-	// 	})
-	// }
-
-	// get avatar image
-	file, fileError := ctx.FormFile("avatar")
-	if fileError != nil {
-		fmt.Println(fileError)
+	stringId := fmt.Sprintf("%v", ctx.Locals("UserId"))
+	userId, primitiveError := primitive.ObjectIDFromHex(stringId)
+	if primitiveError != nil {
 		return utilities.Response(utilities.ResponseParams{
 			Ctx:    ctx,
 			Info:   configuration.ResponseMessages.InternalServerError,
 			Status: fiber.StatusInternalServerError,
 		})
 	}
-	fmt.Println("this is a filename", fmt.Sprintf("./%s", file.Filename))
+
+	// get avatar image
+	file, fileError := ctx.FormFile("avatar")
+	if fileError != nil {
+		return utilities.Response(utilities.ResponseParams{
+			Ctx:    ctx,
+			Info:   configuration.ResponseMessages.MissingData,
+			Status: fiber.StatusBadRequest,
+		})
+	}
 
 	// Save file to root directory:
-	ctx.SaveFile(file, fmt.Sprintf("./%s", file.Filename))
+	ctx.SaveFile(file, fmt.Sprintf("./%s", "static/"+file.Filename))
 
-	// // find user record
-	// rawUserRecord := Instance.Database.Collection("User").FindOne(
-	// 	ctx.Context(),
-	// 	bson.D{{Key: "_id", Value: userId}},
-	// )
-	// userRecord := &User{}
-	// decodeError := rawUserRecord.Decode(userRecord)
-	// if userRecord.ID == "" || decodeError != nil {
-	// 	return utilities.Response(utilities.ResponseParams{
-	// 		Ctx:    ctx,
-	// 		Info:   configuration.ResponseMessages.AccessDenied,
-	// 		Status: fiber.StatusUnauthorized,
-	// 	})
-	// }
+	// find user record
+	rawUserRecord := Instance.Database.Collection("User").FindOne(
+		ctx.Context(),
+		bson.D{{Key: "_id", Value: userId}},
+	)
+	userRecord := &User{}
+	decodeError := rawUserRecord.Decode(userRecord)
+	if userRecord.ID == "" || decodeError != nil {
+		return utilities.Response(utilities.ResponseParams{
+			Ctx:    ctx,
+			Info:   configuration.ResponseMessages.AccessDenied,
+			Status: fiber.StatusUnauthorized,
+		})
+	}
 
 	return utilities.Response(utilities.ResponseParams{
 		Ctx: ctx,
